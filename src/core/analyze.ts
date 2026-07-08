4a0dc21e0cdb6959dbf46ba3ef6a7ed1a4c66932
Felmon <FelmonTesgayFekadu@my.uopeople.edu>
2026-07-07 22:47:00 -0600

    Include configured API wrapper calls in analysis

--- src/core/analyze.ts
+++ src/core/analyze.ts
@@ -1,17 +1,19 @@
 import { buildCoverageReport } from './compare'
 import { scanApiCalls } from './scan-api-calls'
 import { scanHandlers } from './scan-handlers'
+import { scanWrapperCalls } from './scan-wrapper-calls'
 import type { AnalyzerOptions, CoverageReport } from './types'
 
 export async function analyzeProject(options: AnalyzerOptions): Promise<CoverageReport> {
-  const [handlerResult, apiCallResult] = await Promise.all([
+  const [handlerResult, apiCallResult, wrapperCallResult] = await Promise.all([
     scanHandlers(options),
     scanApiCalls(options),
+    scanWrapperCalls(options),
   ])
 
   return buildCoverageReport({
     handlers: handlerResult.handlers,
-    apiCalls: apiCallResult.apiCalls,
-    unsupported: [...handlerResult.unsupported, ...apiCallResult.unsupported],
+    apiCalls: [...apiCallResult.apiCalls, ...wrapperCallResult.apiCalls],
+    unsupported: [...handlerResult.unsupported, ...apiCallResult.unsupported, ...wrapperCallResult.unsupported],
   })
 }
